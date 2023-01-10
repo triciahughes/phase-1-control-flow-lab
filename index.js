8b60b5012c9df5a295dc11bf6ead701db1acc2fc
triciahughes <hughes.a.tricia@gmail.com>
2023-01-09 19:09:41 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let result;
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  } else if (someValue <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (someValue <= 2001) {
+    result = "I will gladly take your thirty bucks.";
+  } else result = "No can do.";
+
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
